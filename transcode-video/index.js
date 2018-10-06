3caddfd91df55f4c6e8fcda3aa705ecbdacfb5fa
Tenzin Wangdhen <sinzin91@gmail.com>
2018-10-06 02:09:36 -0700

    test working

--- transcode-video/index.js
+++ transcode-video/index.js
@@ -2,12 +2,12 @@
 var AWS = require('aws-sdk');
 
 var elasticTranscoder = new AWS.ElasticTranscoder({
-  region = 'us-east-1'
+  region: 'us-east-1'
 });
 
 exports.handler = function(event, context, callback) {
   var key = event.Records[0].s3.object.key;
-  var sourceKey = decodeURIComponen(key.replace(/\+/g, " "));
+  var sourceKey = decodeURIComponent(key.replace(/\+/g, " "));
   var outputKey = sourceKey.split('.')[0];
   console.log('key:', key, sourceKey, outputKey);
   var params = {
